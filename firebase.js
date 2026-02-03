94c5e2f25d612b05509d3f01d3f2b0818187fd4a
Cipherous <elan.mpr@gmail.com>
2026-02-02 19:14:33 -0800

    message

--- firebase.js
+++ firebase.js
@@ -1,11 +1,21 @@
 const admin = require('firebase-admin');
-const serviceAccount =JSON.parse(process.env.FIREBASE_KEY);
+
+if (!process.env.FIREBASE_KEY) {
+  throw new Error("FIREBASE_KEY env variable is missing!");
+}
+
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+} catch (err) {
+  console.error("Failed to parse FIREBASE_KEY:", err);
+  process.exit(1);
+}
 
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-       
-    });
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
 }
 
 module.exports = admin;
